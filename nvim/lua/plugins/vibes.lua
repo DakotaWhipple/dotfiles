6704650aa0177a441da3b0e72d6d573586da5642
Dakota Whipple <dakotajaywhipple@gmail.com>
2026-06-29 09:51:41 -0400

    smart splits kitty

--- nvim/lua/plugins/vibes.lua
+++ nvim/lua/plugins/vibes.lua
@@ -28,21 +28,4 @@ return {
   },
 
   { "LazyVim/LazyVim", opts = { colorscheme = vibe_colorscheme() } },
-
-  -- seamless ctrl-hjkl between kitty splits and nvim splits
-  {
-    "mrjones2014/smart-splits.nvim",
-    lazy = false, -- must run at startup to flag the kitty window as nvim
-    opts = { at_edge = "stop" },
-    keys = {
-      { "<C-h>", function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
-      { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
-      { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
-      { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
-      { "<C-A-h>", function() require("smart-splits").resize_left() end, desc = "Resize split left" },
-      { "<C-A-j>", function() require("smart-splits").resize_down() end, desc = "Resize split down" },
-      { "<C-A-k>", function() require("smart-splits").resize_up() end, desc = "Resize split up" },
-      { "<C-A-l>", function() require("smart-splits").resize_right() end, desc = "Resize split right" },
-    },
-  },
 }
